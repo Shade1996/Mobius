53e87eb8ae502b3eb4ef925fab9dbe6d353fc5d4
Shade1996 <leizhiheng10@gmail.com>
2021-03-15 16:12:15 +0800

    20210315 16:12

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -2,9 +2,10 @@ import  React, { useState, Suspense, useRef }  from 'react'
 import { Canvas, useLoader } from 'react-three-fiber'
 import { Spring } from 'react-spring/renderprops'
 import Bike3d from '../components/Bike3d'
-import { Html } from "drei"
-import { List, ListItem } from '@material-ui/core'
 import DrawerCard from '../components/DrawerCard'
+import { LineChart, Line, ResponsiveContainer, XAxis, Legend } from "recharts"
+import VisibilitySensor from "react-visibility-sensor"
+import { useMap } from './MapPage'
 
 function Loading() {
 	return (
@@ -24,57 +25,113 @@ function Loading() {
 
 const rad2deg = Math.PI / 180
 
-const topToOpacity = (top: number) => {
-  if (top < 0) return 0
-  if (top > 500) return 500
-  return top / 500
+const data = [
+  {
+    name: "Mon",
+    "meters ridden": 4000,
+    "emission saved": 2400,
+  },
+  {
+    name: "Tue",
+    "meters ridden": 3000,
+    "emission saved": 1398,
+  },
+  {
+    name: "Wed",
+    "meters ridden": 2000,
+    "emission saved": 9800,
+  },
+  {
+    name: "Thu",
+    "meters ridden": 2780,
+    "emission saved": 3908,
+  },
+  {
+    name: "Fru",
+    "meters ridden": 1890,
+    "emission saved": 4800,
+  },
+  {
+    name: "Sat",
+    "meters ridden": 2390,
+    "emission saved": 3800,
+  },
+  {
+    name: "Sun",
+    "meters ridden": 3490,
+    "emission saved": 4300,
+  }
+]
+
+const Card: React.FC<{ className?: string }> = ({ children, className }) => {
+    return (
+        <div className={"w-full bg-white bg-opacity-25 mt-4 text-lg rounded-3xl overflow-hidden " + className}>
+            {children}
+        </div>
+    )
 }
 
 export default function HomePage() {
-  const [scrollTop, setScrollTop] = useState(0)
+    const [scrollTop, setScrollTop] = useState(0)
+    const [mapContainer, lng, lat, zoom] = useMap()
 
-  return (
-    <div className="w-screen h-screen">
-      <Spring to={{ top: scrollTop }} config={{ mass: 3 }}>
-        {p => (
-          <>
-            <div className="w-full h-full absolute inset-0 bg-green-400" style={{ opacity: topToOpacity(p.top) }} />
-            <Canvas>
-              <ambientLight intensity={0.5} />
-              <spotLight intensity={0.8} position={[300, 300, 400]} />
-              <group position={[-40, p.top / 10 - 20, -130]} rotation={[0, (p.top / 8 + 45) * rad2deg, 0]}>
-                  <Suspense fallback={<Loading />}>
-                      <Bike3d />
-                  </Suspense>
-              </group>
-            </Canvas>
-          </>
-        )}
-      </Spring>
-      <div
-       className="w-full h-full overflow-x-hidden overflow-y-scroll absolute inset-0"
-       onScroll={e => setScrollTop((e.target as HTMLDivElement).scrollTop)}
-      >
-        <div style={{ height: "65%" }} />
-        <DrawerCard>
-            <div className="text-3xl font-medium mb-5"> 
-              line2
+    return (
+        <div className="w-screen h-screen">
+            <Spring to={{ top: scrollTop }} config={{ mass: 3 }}>
+                {p => (
+                <>
+                    <div className="w-full h-full absolute inset-0 bg-green-400" style={{ opacity: p.top / 500 }} />
+                    <Canvas>
+                    <ambientLight intensity={0.5} />
+                    <spotLight intensity={0.8} position={[300, 300, 400]} />
+                    <group position={[-40, p.top / 10 - 20, -130]} rotation={[0, (p.top / 8 + 45) * rad2deg, 0]}>
+                        <Suspense fallback={<Loading />}>
+                            <Bike3d />
+                        </Suspense>
+                    </group>
+                    </Canvas>
+                </>
+                )}
+            </Spring>
+            <div
+            className="w-full h-full overflow-x-hidden overflow-y-scroll absolute inset-0"
+            onScroll={e => setScrollTop((e.target as HTMLDivElement).scrollTop)}
+            >
+                <div style={{ height: "65%" }} />
+                <DrawerCard>
+                    <div className="text-3xl font-medium mb-5 mt-10"> 
+                        line2
+                    </div>
+                    <div className="text-5xl mb-20 font-bold">
+                        Welcome back
+                    </div>
+                    <div className="text-lg mt-5 font-mono">
+                        Your daily statistics:
+                    </div>
+                    <Card className="p-6">
+                        <VisibilitySensor partialVisibility>
+                            {({ isVisible }) => (
+                                <div style={{ width: window.innerWidth * 0.7, height: 300 }}>
+                                    {isVisible && (
+                                        <LineChart width={window.innerWidth * 0.7} height={300} data={data}>
+                                            <XAxis dataKey="name" tick={{ fontSize: 10 }} tickSize={0} />
+                                            <Legend layout="vertical" />
+                                            <Line type="monotone" dataKey="emission saved" stroke="#8884d8" strokeWidth={2} />
+                                            <Line type="monotone" dataKey="meters ridden" stroke="#82ca9d" strokeWidth={2} />
+                                        </LineChart>
+                                    )}
+                                </div>
+                            )}
+                        </VisibilitySensor>
+                    </Card>
+                    <div className="text-lg mt-10 font-mono">
+                        Current bike location:
+                    </div>
+                    <Card>
+                        <div ref={mapContainer}></div>
+                    </Card>
+                </DrawerCard>
             </div>
-            <div className="text-5xl mb-5 font-bold">
-              Welcome back
-            </div>
-            <div className="text-xl font-light">line 3</div>
-            <div className="inline-block mr-4 text-xl mt-5 font-mono">Routes</div>
-            <div className="inline-block text-xl font-mono">Challenges</div>
-            <div className="w-full bg-white bg-opacity-25 mt-4 p-6 text-lg rounded-3xl">
-              <List  className="text-white space-y-12" >
-                <ListItem className="">Content 123</ListItem>
-                <ListItem className="">123</ListItem>
-                <ListItem className="">123</ListItem>
-              </List>
-            </div>
-        </DrawerCard>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
--- src/page/MapPage.tsx
+++ src/page/MapPage.tsx
@@ -1,45 +1,54 @@
 import React, { useEffect, useRef, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 //@ts-ignore
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
-import DrawerCard from '../components/DrawerCard';
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'
+import DrawerCard from '../components/DrawerCard'
 //@ts-ignore
-mapboxgl.workerClass = MapboxWorker;
+mapboxgl.workerClass = MapboxWorker
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZGUyMzMiLCJhIjoiY2ttN2YzeTFxMHhmbzJ2b2o2cHdtYmN5MyJ9.jMG5SOHve0NgU2aYqSX5BA'
 
+export const useMap = () => {
+    const mapContainer = useRef()
 
-
-export default function MapPage() {
-    const mapContainer = useRef();
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const [lng, setLng] = useState(2.341960)
+    const [lat, setLat] = useState(48.863129)
+    const [zoom, setZoom] = useState(9)
 
     useEffect(() => {
         const map = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [lng, lat],
-        zoom: zoom
-        });
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/light-v10',
+            center: [lng, lat],
+            zoom: zoom
+        })
         map.on('move', () => {
-            //@ts-ignore
-            setLng(map.getCenter().lng.toFixed(4))
-            //@ts-ignore
-            setLat(map.getCenter().lat.toFixed(4))
-            //@ts-ignore
-            setZoom(map.getZoom().toFixed(2))
-            });
-        return () => map.remove();
-        
-        }, [])
+            setLng(parseFloat(map.getCenter().lng.toFixed(4)))
+            setLat(parseFloat(map.getCenter().lat.toFixed(4)))
+            setZoom(parseFloat(map.getZoom().toFixed(2)))
+        })
+
+        const el = document.createElement('div')
+        el.className = "w-0 h-0"
+        el.innerHTML = `
+            <div class="w-4 h-4 bg-blue-500 rounded-full transform -translate-x-20 -translate-y-40"></div>
+        `
+
+        new mapboxgl.Marker(el).setLngLat([lng, lat]).addTo(map)
+
+        return () => {
+            map.remove()
+        }
+    }, [])
+
+    return [mapContainer, lng, lat, zoom] as const
+}
+
+export default function MapPage() {
+    const [mapContainer] = useMap()
 
     return (
         <div className="w-screen h-screen bg-yellow-300">
-            <div className="sidebar">
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-            </div>
-            <div className="map-container" ref={mapContainer} />
+            <div className="absolute inset-0" ref={mapContainer} />
             <div className="w-full h-full overflow-y-scroll absolute inset-0">
                 <div className="h-5/6" />
                 <DrawerCard>
